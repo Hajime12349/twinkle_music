038d18e03a52b121532829bc7495a694a456a909
gunsow <fukudatatsuya911@gmail.com>
2022-10-01 22:20:16 +0900

    CSVローダーを作成した

--- src/Loader.ts
+++ src/Loader.ts
@@ -1,10 +1,29 @@
-import {Star} from "./Star"
+import { Star } from "./Star"
 import { parse } from "papaparse";
 
 /**
- * ファイルパスから星情報をロードする
+ * urlから星情報をロードする
  */
-export const load = async (filepath: string): Promise<Star[]> => {
-
-  return []
+export const load = async (url: string): Promise<Star[]> => {
+  return new Promise<Star[]>((resolve, reject) => {
+    parse(url, {
+      download: true,
+      header: false,
+      newline: '\n',
+      skipEmptyLines: true,
+      complete: (results: any) => {
+        const list = results.data as any[]
+        const stars = list.map((row) => {
+          const power = row[0] as number
+          const freq = row[1] as number
+          const star: Star = {
+            power,
+            freq,
+          }
+          return star
+        })
+        resolve(stars)
+      }
+    });
+  });
 }
